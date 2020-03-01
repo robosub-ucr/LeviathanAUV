dbfe52bc37d8600007020485dc79f2db765a94ad
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2020-02-29 17:12:46 -0800

    Updated

---
+++ main_blueTeensy.cpp
@@ -0,0 +1,16 @@
+#include "mbed.h"
+#include "Servo.h"
+#include <ros.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Bool.h>
+
+//Created by: Steph
+//Description: Communicates with the Red Teensy, ...
+//Microcontroller: Teensy 3.2
+//Date: 02/29/2020 
+
+
+// Thruster code
+// Intialize PWM objects (thrusters)
+
+// 
