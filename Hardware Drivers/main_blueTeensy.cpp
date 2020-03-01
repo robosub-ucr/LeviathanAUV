cfdd1ae0f82578d9e016b327610c48fd204f9f28
Stephanie Cabrera <stephanie.cabrera.333@gmail.com>
2020-02-29 16:52:24 -0800

    Set up the Hardware folder

---
+++ Hardware Drivers/main_blueTeensy.cpp
@@ -0,0 +1,17 @@
+#include "mbed.h"
+#include "Servo.h"
+#include <ros.h>
+#include <std_msgs/Int16.h>
+#include <std_msgs/Bool.h>
+
+//Created by: Steph
+//Description: Controls the thrusters, communicates with the Red Teensy, 
+//             and takes in readings from Hydrophones, Batteries, and 
+//Microcontroller: Teensy 3.2
+//Date: 02/29/2020 
+
+
+// Thruster code
+// Intialize PWM objects (thrusters)
+
+// 
