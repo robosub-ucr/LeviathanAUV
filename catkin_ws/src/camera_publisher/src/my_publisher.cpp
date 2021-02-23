ba0cdfe56b714a104b5d703956ffda815a331b04
mnico005 <mnico005@ucr.edu>
2021-02-22 18:41:54 -0800

    fixed Camera publisher

--- catkin_ws/src/camera_publisher/src/my_publisher.cpp
+++ catkin_ws/src/camera_publisher/src/my_publisher.cpp
@@ -5,8 +5,14 @@
 #include <std_msgs/Int16.h>
 #include <iostream>
 
-void index_callback(const std_msgs::Int16 msg) {
-  cv::VideoCapture cap(msg.data);
+
+int cam_index = 0;
+//bool change = false;
+
+void index_callback(const std_msgs::Int16::ConstPtr& msg) {
+  //cv::VideoCapture cap(msg.data);
+  cam_index = msg->data;
+  //change = true;
 }
 
 int main(int argc, char** argv)
@@ -28,7 +34,10 @@ int main(int argc, char** argv)
   int video_source;
   // Check if it is indeed a number
   if(!(video_sourceCmd >> video_source)) return 1;
-  cv::VideoCapture cap(video_source);
+  cam_index = video_source;
+  //cv::VideoCapture cap(video_source);
+  cv::VideoCapture cap(0);
+  cv::VideoCapture cap2(2);
   // Check if video device can be opened with the given index
   if(!cap.isOpened()) return 1;
   cv::Mat frame;
@@ -36,14 +45,17 @@ int main(int argc, char** argv)
 
   ros::Rate loop_rate(5);
   while (nh.ok()) {
-    cap >> frame;
+    if(cam_index == 0){
+      cap >> frame;
+    }else{
+      cap2>> frame;
+    }
     // Check if grabbed frame is actually full with some content
     if(!frame.empty()) {
       msg = cv_bridge::CvImage(std_msgs::Header(), "bgr8", frame).toImageMsg();
       pub.publish(msg);
       cv::waitKey(1);
     }
-
     ros::spinOnce();
     loop_rate.sleep();
   }
